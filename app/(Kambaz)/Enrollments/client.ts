eb2d3c71e6fa2af5e6693a48b6e958a9e644b8ec
chaitanyajumale <85281310+chaitanyajumale@users.noreply.github.com>
2025-11-17 16:04:28 -0500

    Removed Localhost references 3

--- app/(Kambaz)/Enrollments/client.ts
+++ app/(Kambaz)/Enrollments/client.ts
@@ -4,7 +4,7 @@ const axiosWithCredentials = axios.create({
   withCredentials: true 
 });
 
-const REMOTE_SERVER = process.env.NEXT_PUBLIC_REMOTE_SERVER;
+const REMOTE_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
 const ENROLLMENTS_API = `${REMOTE_SERVER}/api/enrollments`;
 
 export const getMyEnrollments = async () => {
